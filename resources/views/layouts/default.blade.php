ecad2a5148c62eabccca6120e06a9377c2e933a1
JIA LIU <timspace1988@hotmail.com>
2017-06-01 05:09:35 +0000

    Finish static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>@yield('title', 'My Weibo')</title><!--second param is a default value-->
+  </head>
+  <body>
+    @yield('content')
+  </body>
+</html>
