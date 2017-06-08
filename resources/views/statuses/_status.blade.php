ae1c372abc8ae3f2aecd7ecde003091056a49afd
JIA LIU <timspace1988@hotmail.com>
2017-06-08 17:29:39 +1000

    internal error debug 3

--- resources/views/statuses/_status.blade.php
+++ resources/views/statuses/_status.blade.php
@@ -9,11 +9,5 @@
     {{$status->created_at->diffForHumans()}}<!--difForHumans will display the date as 'xx days ago' format-->
   </span>
   <span class="content">{{$status->content}}</span>
-  @can('destroy', $status)
-    <form action="{{route('statuses.destroy', $status->id)}}" method="post">
-      {{csrf_field()}}
-      {{method_field('DELETE')}}
-      <button type="submit" class="btn btn-sm btn-danger status-delete-btn">Delete</button>
-    </form>
-  @endcan
+
 </li>
