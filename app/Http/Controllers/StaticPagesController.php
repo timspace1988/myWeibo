69dcfd4925e5bc6a889a7de9836c71d2e690974f
JIA LIU <timspace1988@hotmail.com>
2017-07-18 12:17:00 +1000

    debug

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -21,9 +21,9 @@ class StaticPagesController extends Controller
 
     public function home(){
         $feed_items = [];
-        if(Auth::check()){
-            $feed_items = Auth::user()->feed()->paginate(30);
-        }
+        // if(Auth::check()){
+        //     $feed_items = Auth::user()->feed()->paginate(30);
+        // }
         $uploadManager = $this->manager;
         return view('static_pages/home', compact('feed_items', 'uploadManager'));
     }
