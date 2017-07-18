5215b6a0fd281207e88f8a28985fb5aa49ebcec2
JIA LIU <timspace1988@hotmail.com>
2017-07-18 13:39:10 +1000

    debug

--- config/upload.php
+++ config/upload.php
@@ -2,5 +2,6 @@
 
 return [
     'storage' => env('CLOUD_STORAGE', 'local'),
-    'webpath' => env('AWS_URL', '/upload'),
+    //'webpath' => env('AWS_URL', '/upload'),
+    'webpath' => 'https://s3.us-east-2.amazonaws.com/jiablogheroku',
 ];
